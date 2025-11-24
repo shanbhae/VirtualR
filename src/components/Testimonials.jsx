87b9ebcbbac8fe9ccdc4141508e9b20b46c140b8
Zeeshan Iqbal <165188741+shanbhae@users.noreply.github.com>
2025-11-24 22:57:49 +0500

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,42 +1,29 @@
-import { CheckCircle2 } from "lucide-react";
-import { pricingOptions } from "../constants/Index";
+import { testimonials } from "../constants";
 
-const Pricing = () => {
+const Testimonials = () => {
   return (
-    <div className="mt-20">
-      <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-8 tracking-wide">
-        Pricing
+    <div className="mt-20 tracking-wide">
+      <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-10 lg:my-20">
+        What People are saying
       </h2>
-      <div className="flex flex-wrap">
-        {pricingOptions.map((option, index) => (
-          <div key={index} className="w-full sm:w-1/2 lg:w-1/3 p-2">
-            <div className="p-10 border border-neutral-700 rounded-xl">
-              <p className="text-4xl mb-8">
-                {option.title}
-                {option.title === "Pro" && (
-                  <span className="bg-gradient-to-r from-orange-500 to-red-400 text-transparent bg-clip-text text-xl mb-4 ml-2">
-                    (Most Popular)
+      <div className="flex flex-wrap justify-center">
+        {testimonials.map((testimonial, index) => (
+          <div key={index} className="w-full sm:w-1/2 lg:w-1/3 px-4 py-2">
+            <div className="bg-neutral-900 rounded-md p-6 text-md border border-neutral-800 font-thin">
+              <p>{testimonial.text}</p>
+              <div className="flex mt-8 items-start">
+                <img
+                  className="w-12 h-12 mr-6 rounded-full border border-neutral-300"
+                  src={testimonial.image}
+                  alt=""
+                />
+                <div>
+                  <h6>{testimonial.user}</h6>
+                  <span className="text-sm font-normal italic text-neutral-600">
+                    {testimonial.company}
                   </span>
-                )}
-              </p>
-              <p className="mb-8">
-                <span className="text-5xl mt-6 mr-2">{option.price}</span>
-                <span className="text-neutral-400 tracking-tight">/Month</span>
-              </p>
-              <ul>
-                {option.features.map((feature, index) => (
-                  <li key={index} className="mt-8 flex items-center">
-                    <CheckCircle2 />
-                    <span className="ml-2">{feature}</span>
-                  </li>
-                ))}
-              </ul>
-              <a
-                href="#"
-                className="inline-flex justify-center items-center text-center w-full h-12 p-5 mt-20 tracking-tight text-xl hover:bg-orange-900 border border-orange-900 rounded-lg transition duration-200"
-              >
-                Subscribe
-              </a>
+                </div>
+              </div>
             </div>
           </div>
         ))}
@@ -45,4 +32,4 @@ const Pricing = () => {
   );
 };
 
-export default Pricing;
+export default Testimonials;
